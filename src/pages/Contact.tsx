85cf09713d65bbf2f8c8184af0923f44b0e28e4e
Murilo Trigo <murilo@webandflow.coop>
2024-10-21 17:50:48 -0400

    [WIP] Adds a contact form that forwards to the admin@webandflow.coop email

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,7 +3,33 @@ const Contact = () => {
     <div className="container mt-5">
       <h1 className="mt-5 brush-title">Contact Us</h1>
       <div className="body-content">
-        <p>This is the contact page styled with Bootstrap!</p>
+        <section className="contact-section">
+          <div className="contact-intro">
+            <p className="contact-description">
+              Fill out the form below and we'll get back to you
+            </p>
+          </div>
+          <form className="contact-form" action="https://api.web3forms.com/submit" method="POST">
+            <input type="hidden" name="access_key" value="417c756a-b2e9-41b8-bafd-977ba76ae8ed" />
+            <input type="hidden" name="subject" value="New message from the website" />
+            <input type="hidden" name="from_name" value="WF Contact Form" />
+            <div className="form-group-container">
+              <div className="form-group">
+                <label htmlFor="name" className="form-label">Name</label>
+                <input id="name" name="name" className="form-input" placeholder="Your name" type="text" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email" className="form-label">Email</label>
+                <input id="email" name="email" className="form-input" placeholder="Your email" type="email" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message" className="form-label">Message</label>
+                <textarea className="form-textarea" id="message" name="message" placeholder="Your message"></textarea>
+              </div>
+            </div>
+            <button className="form-submit" type="submit">Send Message</button>
+          </form>
+        </section>
       </div>
     </div>
   );
