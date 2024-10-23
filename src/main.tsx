02fc3543ec77f8f58865da349a67e721cc761514
Jessica Trigo <jessica@webandflow.coop>
2024-10-23 19:35:56 -0400

    Adds /webandflow/ reference to BrowserRouter and vite.config

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "./App.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/webandflow">
       <App />
     </BrowserRouter>
   </StrictMode>
